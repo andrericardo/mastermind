8487441695929261f52408d10159aa152a42e5e4
André Ricardo <andric87@gmail.com>
2022-10-29 12:10:21 +0100

    Implement right and wrong positions result

--- core/rules.py
+++ core/rules.py
@@ -9,7 +9,15 @@ class GuessResult:
 
 
 def check_guess(solution, guess) -> GuessResult:
-    is_winner = solution == guess
-    right_position = len(guess)
+    right_position = 0
     wrong_position = 0
+
+    for (solution_peg, guess_peg) in zip(solution, guess):
+        if solution_peg == guess_peg:
+            right_position += 1
+        elif guess_peg in solution:
+            wrong_position += 1
+
+    is_winner = right_position == len(guess)
+
     return GuessResult(is_winner, right_position, wrong_position)
--- tests/test_rules.py
+++ tests/test_rules.py
@@ -2,46 +2,60 @@ from core.constants import ColorPeg
 from core.rules import check_guess
 
 
-def test_correct_guess_is_true():
-    solution = (ColorPeg.RED, ColorPeg.BLUE, ColorPeg.YELLOW, ColorPeg.GREEN)
-    guess = solution
+class TestCheckGuess:
 
-    result = check_guess(solution, guess)
+    # It would be possible to combine these is_winner tests by using
+    # @pytest.mark.parametrize but would make them harder to read
+    def test_correct_guess_is_true(self):
+        solution = (ColorPeg.RED, ColorPeg.BLUE, ColorPeg.YELLOW, ColorPeg.GREEN)
+        guess = solution
 
-    assert result.is_winner
+        result = check_guess(solution, guess)
 
+        assert result.is_winner
 
-def test_incorrect_order_is_false():
-    solution = (ColorPeg.RED, ColorPeg.BLUE, ColorPeg.YELLOW, ColorPeg.GREEN)
-    guess = (ColorPeg.BLUE, ColorPeg.RED, ColorPeg.YELLOW, ColorPeg.GREEN)
+    def test_incorrect_order_is_false(self):
+        solution = (ColorPeg.RED, ColorPeg.BLUE, ColorPeg.YELLOW, ColorPeg.GREEN)
+        guess = (ColorPeg.BLUE, ColorPeg.RED, ColorPeg.YELLOW, ColorPeg.GREEN)
 
-    result = check_guess(solution, guess)
+        result = check_guess(solution, guess)
 
-    assert not result.is_winner
+        assert not result.is_winner
 
+    def test_incorrect_color_is_false(self):
+        solution = (ColorPeg.RED, ColorPeg.BLUE, ColorPeg.YELLOW, ColorPeg.GREEN)
+        guess = (ColorPeg.MAGENTA, ColorPeg.BLUE, ColorPeg.YELLOW, ColorPeg.GREEN)
 
-def test_incorrect_color_is_false():
-    solution = (ColorPeg.RED, ColorPeg.BLUE, ColorPeg.YELLOW, ColorPeg.GREEN)
-    guess = (ColorPeg.MAGENTA, ColorPeg.BLUE, ColorPeg.YELLOW, ColorPeg.GREEN)
+        result = check_guess(solution, guess)
 
-    result = check_guess(solution, guess)
+        assert not result.is_winner
 
-    assert not result.is_winner
+    def test_winner(self):
+        solution = (ColorPeg.RED, ColorPeg.BLUE, ColorPeg.YELLOW, ColorPeg.GREEN)
+        guess = solution
 
+        result = check_guess(solution, guess)
 
-def test_colors_right_position_for_winner():
-    solution = (ColorPeg.RED, ColorPeg.BLUE, ColorPeg.YELLOW, ColorPeg.GREEN)
-    guess = solution
+        assert result.is_winner
+        assert result.right_position == 4
+        assert result.wrong_position == 0
 
-    result = check_guess(solution, guess)
+    def test_position_swapped(self):
+        solution = (ColorPeg.RED, ColorPeg.BLUE, ColorPeg.YELLOW, ColorPeg.GREEN)
+        guess = (ColorPeg.BLUE, ColorPeg.RED, ColorPeg.YELLOW, ColorPeg.GREEN)
 
-    assert result.right_position == 4
+        result = check_guess(solution, guess)
 
+        assert not result.is_winner
+        assert result.right_position == 2
+        assert result.wrong_position == 2
 
-def test_colors_wrong_position_for_winner():
-    solution = (ColorPeg.RED, ColorPeg.BLUE, ColorPeg.YELLOW, ColorPeg.GREEN)
-    guess = solution
+    def test_all_position_swapped(self):
+        solution = (ColorPeg.RED, ColorPeg.BLUE, ColorPeg.YELLOW, ColorPeg.GREEN)
+        guess = (ColorPeg.BLUE, ColorPeg.RED, ColorPeg.GREEN, ColorPeg.YELLOW)
 
-    result = check_guess(solution, guess)
+        result = check_guess(solution, guess)
 
-    assert result.wrong_position == 0
+        assert not result.is_winner
+        assert result.right_position == 0
+        assert result.wrong_position == 4
