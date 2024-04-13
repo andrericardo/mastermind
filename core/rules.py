10118df4e9a93a9e41c8c1ca27c5e3704ef1ef96
André Ricardo <andric87@gmail.com>
2024-04-13 17:04:06 +0100

    Update dependencies with vulnerabilities and run black.

--- core/rules.py
+++ core/rules.py
@@ -12,7 +12,7 @@ def check_guess(solution, guess) -> GuessResult:
     right_position = 0
     wrong_position = 0
 
-    for (solution_peg, guess_peg) in zip(solution, guess):
+    for solution_peg, guess_peg in zip(solution, guess):
         if solution_peg == guess_peg:
             right_position += 1
         elif guess_peg in solution:
